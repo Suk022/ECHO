9f1a2f77b51b3d5ba2ad6dfb7cb68666ae7a814f
Sudhanshu <122535400+Suk022@users.noreply.github.com>
2026-04-05 11:50:13 +0530

    Fix Config files for frontend

--- aria/js/config.js
+++ aria/js/config.js
@@ -1,6 +1,8 @@
-// Configuration 
+// Configuration
+const IS_LOCALHOST = ['localhost', '127.0.0.1'].includes(window.location.hostname);
+
 const CONFIG = {
-  BACKEND_URL: window.location.origin.includes('vercel.app') || window.location.origin.includes('echo-psi-eosin') 
-    ? 'https://echo-ko7o.onrender.com' 
-    : 'http://127.0.0.1:8000'
+  BACKEND_URL: IS_LOCALHOST
+    ? 'http://127.0.0.1:8000'
+    : 'https://echo-wsu5.onrender.com'
 };
